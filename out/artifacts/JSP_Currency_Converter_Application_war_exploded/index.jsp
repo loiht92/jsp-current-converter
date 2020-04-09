4f5b350f6e312a5389cf04f83aac9a6e3ab17a66
loiminh <loiho11082015@gmail.com>
2020-04-09 07:46:26 +0700

    add file

---
+++ out/artifacts/JSP_Currency_Converter_Application_war_exploded/index.jsp
@@ -0,0 +1,38 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: holoi
+  Date: 4/8/20
+  Time: 17:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Currency Converter</title>
+    <style>
+      .form {
+        margin: 20px 20px 20px 20px;
+        border: 1px solid blue;
+      }
+      .form h1 {
+        color: blue;
+        font-size: 30px;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+  <form class="form" action="${pageContext.request.contextPath}/converter" method="post">
+    <h1>Currency Converter</h1>
+    <label>Rate:</label><br/>
+    <label>
+      <input type="text" name="rate" placeholder="Enter a rate" value="22000">
+    </label>
+    <label>USD:</label><br/>
+    <label>
+      <input type="text" name="usd" placeholder="Enter usd" value="0">
+    </label>
+    <input type="submit" id="submit" value="Converter">
+  </form>
+  </body>
+</html>
---
+++ src/CurrencyConverter.java
@@ -0,0 +1,28 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Currency-Converter",urlPatterns = "/converter")
+public class CurrencyConverter extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        float rate = Float.parseFloat(request.getParameter("rate"));
+        float usd = Float.parseFloat(request.getParameter("usd"));
+
+        float vnd = rate * usd;
+
+        request.setAttribute("rate", rate);
+        request.setAttribute("usd", usd);
+        request.setAttribute("vnd", vnd);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("converter.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
